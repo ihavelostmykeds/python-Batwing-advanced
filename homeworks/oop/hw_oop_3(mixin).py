de27d9ea0a8621a4ab8d51e735b7f19180ac021d
Oleksandr Zasanskyi <glorylastsforever@ukr.net>
2022-05-24 16:25:17 +0300

    task 3 mixin version

--- homeworks/oop/hw_oop_3(mixin).py
+++ homeworks/oop/hw_oop_3(mixin).py
@@ -1,4 +1,12 @@
-class Profile:
+class ListMixin:
+    def __str__(self):
+        lst = []
+        for value in self.__dict__.values():
+            lst.append(value)
+        return lst.__repr__()
+
+
+class Profile(ListMixin):
     def __init__(self, name, last_name, phone_number, address, email, birthday, age, sex):
         self.name = name
         self.last_name = last_name
@@ -8,12 +16,6 @@ class Profile:
         self.birthday = birthday
         self.age = age
         self.sex = sex
-
-    def __str__(self):
-        lst = []
-        for value in self.__dict__.values():
-            lst.append(value)
-        return lst.__repr__()
 
 
 anton = Profile('Anton', 'Gera', '0730333333', 'Lviv', 'test@test.com', '29.07.1995', '26', 'male')
